9a53f813c85660559200f95a7f992926c7fa99e1
Lauri Heiskanen <lauri@nimble.fi>
2025-08-30 15:56:28 +0300

    ghostty tab fix attempt, few gopls keyboard shortcuts defined

--- .config/nvim/lsp/gopls.lua
+++ .config/nvim/lsp/gopls.lua
@@ -1,7 +1,19 @@
 vim.lsp.config["gopls"] = {
       cmd = { "gopls" },
       root_markers= { "go.work", "go.mod", "go.sum", ".git" },
-      filetypes = { "go", "gomod", "gowork", "gotmpl" }
+      filetypes = { "go", "gomod", "gowork", "gotmpl" },
       settings = {},
+      on_attach = function(client, bufnr)
+    local opts = { buffer = bufnr, noremap = true, silent = true }
+
+    -- Go to definition
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+
+    -- Optional extras:
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  end,
 }
 
