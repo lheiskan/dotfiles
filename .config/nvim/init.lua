4f77b57d23abce0f77a1cb8e8318142f12487639
Lauri Heiskanen <lauri@nimble.fi>
2026-02-07 11:03:26 +0200

    config updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,11 +9,29 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
 vim.keymap.set("n", "<leader>w", ":write<CR>")
 vim.keymap.set("n", "<leader>q", ":quit<CR>")
+vim.keymap.set("n", "<leader>m", ":make<CR>")
 vim.keymap.set("n", "gq", "mggg=Gg`g")
-vim.keymap.set({ "n", "v", "x" }, '<leader>y', '"+y')
-vim.keymap.set({ "n", "v", "x" }, '<leader>d', '"+d')
-vim.keymap.set({ "n", "v", "x" }, '<leader>s', ':e #<CR>')
-vim.keymap.set({ "n", "v", "x" }, '<leader>S', ':sf #<CR>')
+vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y')
+vim.keymap.set({ "n", "v", "x" }, "<leader>d", '"+d')
+vim.keymap.set({ "n", "v", "x" }, "<leader>s", ":e #<CR>")
+vim.keymap.set({ "n", "v", "x" }, "<leader>S", ":sf #<CR>")
+
+vim.keymap.set("n", "<leader>1", function()
+	local file = vim.fn.stdpath("config") .. "/init.lua"
+	vim.cmd.edit(file)
+end, {
+	desc = "Edit init.lua",
+})
+vim.keymap.set("n", "<leader>!", function()
+	MiniPick.builtin.files(nil, { source = { cwd = vim.fn.stdpath("config") } })
+end, { desc = "Find and pick from nvim configs" })
+vim.keymap.set("n", "<leader>2", function()
+	local file = vim.fn.expand("~/github/notes/notes/daily/" .. os.date("%Y-%m-%d") .. ".md")
+	vim.cmd.edit(file)
+end, { desc = "Open today's note" })
+vim.keymap.set("n", "<leader>@", function()
+	MiniPick.builtin.files(nil, { source = { cwd = vim.fn.expand("~/github/notes") } })
+end, { desc = "Find and pick from notes" })
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
@@ -22,8 +40,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/Saghen/blink.cmp" },
 	{ src = "https://github.com/rafamadriz/friendly-snippets.git" },
-}
-)
+})
 
 require("nvim-treesitter.configs").setup({
 	ensure_installed = { "go", "svelte", "typescript", "html", "javascript" },
@@ -34,10 +51,10 @@ require("nvim-treesitter.configs").setup({
 	auto_install = false,
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
+		if client:supports_method("textDocument/completion") then
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
 	end,
@@ -55,4 +72,6 @@ vim.keymap.set("n", "<leader>b", ":Pick buffers<CR>")
 
 vim.lsp.enable({ "lua_ls", "tinymist" })
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end)
+vim.keymap.set("n", "<leader>ld", function()
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end)
